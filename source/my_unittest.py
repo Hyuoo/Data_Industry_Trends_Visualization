5a5280e0dce2c91081b0b5cd369ba1090690f44c
Hyuoo <43870227+Hyuoo@users.noreply.github.com>
2023-04-26 19:12:41 +0900

    Update my_unittest.py

--- source/my_unittest.py
+++ source/my_unittest.py
@@ -10,6 +10,10 @@ class my_test(unittest.TestCase):
     def test_case2(self):
         ans = dec_to_hex(100)
         self.assertEqual(ans, "%#x"%100)
+    
+    def test_case3(self):
+        ans = dec_to_hex(0)
+        self.asserNotEqual(ans, "0")
 
 if __name__=="__main__":
     unittest.main()
